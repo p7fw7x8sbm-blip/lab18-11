3fa678908a988e4540280e36b50b0342d79512d3
p7fw7x8sbm-blip <p7fw7x8sbm@privaterelay.appleid.com>
2025-11-19 15:40:40 +0800

    Update expense_tracker.py

--- expense_tracker.py
+++ expense_tracker.py
@@ -45,7 +45,7 @@ if not st.session_state.expenses.empty:
     category_totals = st.session_state.expenses.groupby('Category')['Amount'].sum
     category_totals = st.session_state.expense.groupby('Category')['Amount'].sum()
   
-    fig,ax = plt.sbplot(figsize = (10,6)
+    fig,ax = plt.sbplot(figsize = (10,6))
     ax.pie(category_totals.values, labels = category_totals.index, autopct = '%1.1f%')
     ax.set_title("Expense by Category")
     st.pyplot(fig)
